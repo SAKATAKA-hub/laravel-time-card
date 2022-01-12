f99d74002e045b4d7f19987e1cedb8fc05ed8562
SAKA TAKA <aek1214@yahoo.co.jp>
2022-01-12 17:20:17 +0900

    heroku設定

--- config/database.php
+++ config/database.php
@@ -46,13 +46,20 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+
+            'host' => $db['host'],
+            'database' => ltrim($db['path'],'/'),
+            'username' => $db['user'],
+            'password' => $db['pass'],
+
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
+
             'charset' => 'utf8mb4',
+            'port' => env('DB_PORT', '3306'),
+            'unix_socket' => env('DB_SOCKET', ''),
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
